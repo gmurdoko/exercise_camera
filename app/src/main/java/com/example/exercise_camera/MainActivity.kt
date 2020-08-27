4c21205091d48263dd876731aa79b2ec23728d3b
Galang Cahyo Murdoko <galangcahyomurdoko@gmail.com>
2020-08-27 17:06:41 +0700

    write and read file

--- app/src/main/java/com/example/exercise_camera/MainActivity.kt
+++ app/src/main/java/com/example/exercise_camera/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.exercise_camera
 
 import android.app.Activity
 import android.content.Intent
+import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
@@ -11,15 +12,19 @@ import android.os.Environment
 import android.provider.MediaStore
 import android.view.View
 import androidx.core.content.FileProvider
+import androidx.core.net.toFile
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.IOException
 import java.net.URI
+import java.nio.file.FileStore
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    val SELECT_FILE_FROM_STORAGE = 33
 
     val OPEN_CAMERA_REQUEST_CODE = 23
     lateinit var currentPhotoPath : String
@@ -29,6 +34,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun browseFile(view: View){
+        val selectFileIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+//        val selectFileIntent = Intent(Intent.ACTION_PICK, )
+        startActivityForResult(selectFileIntent, SELECT_FILE_FROM_STORAGE)
+    }
+
     fun openCamera(view: View){
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         cameraIntent.resolveActivity(packageManager)
@@ -54,13 +66,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == OPEN_CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK){
             val imageBitmap = BitmapFactory.decodeFile(photoFile.absolutePath)
             imageView.setImageBitmap(imageBitmap)
         }
+
+        if(requestCode == SELECT_FILE_FROM_STORAGE && resultCode == Activity.RESULT_OK){
+//            read uri
+//            val uriFile = data?.data
+//            imageView.setImageURI(uriFile)
+            val originalPath : String? = getOriginalPathFromUri(data?.data!!)
+            //Image File
+            val imageFile = File(originalPath)
+
+            val imageBitmap = BitmapFactory.decodeFile(imageFile.absolutePath)
+            imageView.setImageBitmap(imageBitmap)
+        }
+
+    }
+    fun getOriginalPathFromUri(contentUri: Uri): String?{
+        var originalPath: String? = null
+        val projection =
+            arrayOf(MediaStore.Images.Media.DATA)
+        val cursor: Cursor? = contentResolver.query(contentUri, projection, null, null, null)
+        if (cursor?.moveToFirst()!!) {
+            val columnIndex: Int = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            originalPath = cursor.getString(columnIndex)
+        }
+        return originalPath
     }
 }
